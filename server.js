d504a7a81262eb1596740dcf33009326daf29403
tibirabotto <tibira.botto@gmail.com>
2023-10-25 12:03:36 -0400

    fixed listen call function

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ const connection = net.createServer((socket) => {
 const PORT = 3000;
 const HOST = '127.0.0.1';
 
-server.listen(PORT, HOST, () => {
+connection.listen(PORT, HOST, () => {
     console.log(`Server listening on ${HOST}:${PORT}`);
 });
